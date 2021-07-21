ec7521e7be23af507de626df647bf1e2ab200c3a
anand <anand.kumar@swoopos.com>
2021-07-21 13:18:07 +0530

    First init

--- app/src/main/java/com/droidhubworld/crashloggersample/BaseApp.java
+++ app/src/main/java/com/droidhubworld/crashloggersample/BaseApp.java
@@ -9,7 +9,6 @@ public class BaseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         if (BuildConfig.DEBUG) {
             //initialise reporter with external path
             CrashLogReporter.initialize(this);
